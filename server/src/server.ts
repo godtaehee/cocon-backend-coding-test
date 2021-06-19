cfe52944ac2add4a90f7d7f741f107b7000cae97
taehee <wonderfulhuman@naver.com>
2021-06-20 04:46:44 +0900

    Add dotenv and Success hide MongoDB URL and Port

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,12 @@
 import express from "express";
 import {connect} from "./database/database";
 import dataRouter from "./router/dataRouter";
+import dotenv from "dotenv";
 
 const app = express();
-const port = 5002;
+const port = process.env.PORT;
+
+dotenv.config({path:'./config/.env'});
 
 app.use(express.json());
 app.use("/", dataRouter);
