3c1b1969c68208588045205e8f4858de5ad96afb
taehee <wonderfulhuman@naver.com>
2021-06-20 00:55:23 +0900

    Success MongoDB Connect And tsc-watch And Express Setting

---
+++ server/src/database/database.ts
@@ -0,0 +1,28 @@
+import * as Mongoose from "mongoose";
+let database: Mongoose.Connection;
+export const connect = () => {
+    // add your own uri below
+    const uri = "mongodb+srv://root:root@cocon-backend-coding-te.9kn59.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+    if (database) {
+        return;
+    }
+    Mongoose.connect(uri, {
+        useNewUrlParser: true,
+        useFindAndModify: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+    });
+    database = Mongoose.connection;
+    database.once("open", async () => {
+        console.log("Connected to database");
+    });
+    database.on("error", () => {
+        console.log("Error connecting to database");
+    });
+};
+export const disconnect = () => {
+    if (!database) {
+        return;
+    }
+    Mongoose.disconnect();
+};
